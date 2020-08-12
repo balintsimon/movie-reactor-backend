49c1de26ed2459e0c00b6db2aacb79811a10ec17
dr. Simon Bálint <balladin0@gmail.com>
2020-08-12 09:10:55 +0200

    Add Spring Security

---
+++ api-gateway/src/main/java/com/drbsimon/apigateway/security/CustomUserDetailsService.java
@@ -0,0 +1,46 @@
+package com.drbsimon.apigateway.security;
+
+import com.drbsimon.apigateway.entity.Visitor;
+import com.drbsimon.apigateway.repository.VisitorRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private VisitorRepository visitors;
+
+    public CustomUserDetailsService(VisitorRepository visitors) {
+        this.visitors = visitors;
+    }
+
+    /**
+     * Loads the user from the DB and converts it to Spring Security's internal User object.
+     * Spring will call this code to retrieve a user upon login from the DB.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Visitor visitor = visitors.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username: " + username + " not found"));
+        return new User(visitor.getUsername(), visitor.getPassword(),
+                visitor.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+    }
+
+    public String findLoggedInUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return username;
+    }
+}
---
+++ api-gateway/src/main/java/com/drbsimon/apigateway/security/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.drbsimon.apigateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@CrossOrigin
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // TODO: delete permit all upon endpoints revision
+                .antMatchers("/*").permitAll()
+                // TODO: revise endpoints
+//                .antMatchers("/auth/*").permitAll()
+//                .antMatchers(HttpMethod.GET, "/watchlist").hasRole("USER")
+//                .antMatchers(HttpMethod.POST, "/watchlist/save/*").hasRole("USER")
+//                .antMatchers(HttpMethod.DELETE, "/watchlist/delete/*").hasRole("USER")
+//                .antMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET, "/me").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET, "/schedule").permitAll()
+//                .antMatchers(HttpMethod.GET, "/scheduled-movies").permitAll()
+//                .antMatchers(HttpMethod.GET, "/show/*").permitAll()
+//                .antMatchers(HttpMethod.PUT, "/show/*").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.POST,"/reservation/seats").hasRole("USER")
+//                .antMatchers(HttpMethod.GET,"/reservation/seats").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.DELETE,"/reservation/delete").hasRole("ADMIN")
+                .anyRequest().denyAll() // anything else is denied
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowCredentials(true);
+        // configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-*", "Origin", "Content-type", "Accept", "Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        // configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
