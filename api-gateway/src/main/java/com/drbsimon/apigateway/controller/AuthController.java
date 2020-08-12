14e144ef12b619b33dbd933469ecf6d9518f7446
dr. Simon Bálint <balladin0@gmail.com>
2020-08-12 12:37:17 +0200

    Refactor registration to service

--- api-gateway/src/main/java/com/drbsimon/apigateway/controller/AuthController.java
+++ api-gateway/src/main/java/com/drbsimon/apigateway/controller/AuthController.java
@@ -1,21 +1,12 @@
 package com.drbsimon.apigateway.controller;
 
-import com.drbsimon.apigateway.entity.Visitor;
-import com.drbsimon.apigateway.model.Gender;
 import com.drbsimon.apigateway.model.UserCredentials;
 import com.drbsimon.apigateway.model.VisitorLoginService;
-import com.drbsimon.apigateway.repository.VisitorRepository;
-import com.drbsimon.apigateway.security.DataValidatorService;
-import com.drbsimon.apigateway.security.JwtTokenServices;
+import com.drbsimon.apigateway.model.VisitorRegisterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.*;
 
 @CrossOrigin(origins = "${main.route}")
 @RestController
@@ -23,11 +14,8 @@ import java.util.*;
 @RequiredArgsConstructor
 @Slf4j
 public class AuthController {
-    private final VisitorRepository visitorRepository;
-    private final JwtTokenServices jwtTokenServices;
-    private final DataValidatorService dataValidator;
-    private final PasswordEncoder passwordEncoder;
     private final VisitorLoginService visitorLoginService;
+    private final VisitorRegisterService visitorRegisterService;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody UserCredentials userCredentials) {
@@ -37,83 +25,7 @@ public class AuthController {
     // TODO: rearrange to one line
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody UserCredentials userCredentials) {
-        String username = userCredentials.getUsername();
-        String password = userCredentials.getPassword();
-        String firstname = userCredentials.getFirstname();
-        String lastname = userCredentials.getLastname();
-        String email = userCredentials.getEmail();
-        Gender gender = userCredentials.getGender();
-
-        Map<Object, Object> model = new HashMap<>();
-        List<String> errorList = new ArrayList<>();
-
-        if (visitorRepository.findByUsername(username).isPresent()) {
-            model.put("correct", false);
-            model.put("msg", "Username already exists! Please choose a different username!");
-            return ResponseEntity.ok(model);
-        }
-        if (!dataValidator.isValidUsername(username, errorList)) {
-            String error = String.join(" , ", errorList);
-            String errorMessage = "Username should have " + error + "!";
-            model.put("correct", false);
-            model.put("msg", errorMessage);
-            return ResponseEntity.ok(model);
-        }
-
-        if (!dataValidator.isValidPassword(password, errorList)) {
-            String error = String.join(" , ", errorList);
-            String errorMessage = "Password should contain " + error + "!";
-            model.put("correct", false);
-            model.put("msg", errorMessage);
-            return ResponseEntity.ok(model);
-        }
-
-        if (!dataValidator.isValidName(firstname, errorList)) {
-            String error = String.join(" , ", errorList);
-            String errorMessage = "Firstname " + error + "!";
-            model.put("correct", false);
-            model.put("msg", errorMessage);
-            return ResponseEntity.ok(model);
-        }
-
-        if (!dataValidator.isValidName(lastname, errorList)) {
-            String error = String.join(" , ", errorList);
-            String errorMessage = "Lastname " + error + "!";
-            model.put("correct", false);
-            model.put("msg", errorMessage);
-            return ResponseEntity.ok(model);
-        }
-
-        if (!dataValidator.isValidEmail(email, errorList)) {
-            model.put("correct", false);
-            model.put("msg", "Not valid email!");
-            return ResponseEntity.ok(model);
-        }
-
-        if (!dataValidator.isValidGender(gender, errorList)) {
-            model.put("correct", false);
-            model.put("msg", "Not related data!");
-            return ResponseEntity.ok(model);
-        }
-
-        Visitor newVisitor = Visitor.builder()
-                .username(username)
-                .password(passwordEncoder.encode(password))
-                .firstname(firstname)
-                .lastname(lastname)
-                .email(email)
-                .gender(gender)
-                .roles(Collections.singletonList("ROLE_USER"))
-                .build();
-        visitorRepository.save(newVisitor);
-
-        List<String> roles = Collections.singletonList("ROLE_USER");
-        String token = jwtTokenServices.createToken(username, roles);
-        model.put("correct", true);
-        model.put("username", username);
-        model.put("roles", roles);
-        model.put("token", token);
-        return ResponseEntity.ok(model);
+        return visitorRegisterService.registerUser(userCredentials);
     }
 }
 
--- api-gateway/src/main/java/com/drbsimon/apigateway/model/VisitorRegisterService.java
+++ api-gateway/src/main/java/com/drbsimon/apigateway/model/VisitorRegisterService.java
@@ -1,2 +1,113 @@
-package com.drbsimon.apigateway.model;public class VisitorRegisterService {
+package com.drbsimon.apigateway.model;
+
+import com.drbsimon.apigateway.entity.Visitor;
+import com.drbsimon.apigateway.repository.VisitorRepository;
+import com.drbsimon.apigateway.security.DataValidatorService;
+import com.drbsimon.apigateway.security.JwtTokenServices;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Data
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class VisitorRegisterService {
+    private final VisitorRepository visitorRepository;
+    private final JwtTokenServices jwtTokenServices;
+    private final DataValidatorService dataValidator;
+    private final PasswordEncoder passwordEncoder;
+    private final VisitorLoginService visitorLoginService;
+
+    public ResponseEntity registerUser(UserCredentials userCredentials) {
+        String username = userCredentials.getUsername();
+        String password = userCredentials.getPassword();
+        String firstname = userCredentials.getFirstname();
+        String lastname = userCredentials.getLastname();
+        String email = userCredentials.getEmail();
+        Gender gender = userCredentials.getGender();
+
+        Map<Object, Object> model = new HashMap<>();
+
+        ResponseEntity failedRegisterMessage = validateRegistryData(userCredentials);
+        if (failedRegisterMessage != null) return failedRegisterMessage;
+
+        Visitor newVisitor = Visitor.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .firstname(firstname)
+                .lastname(lastname)
+                .email(email)
+                .gender(gender)
+                .roles(Collections.singletonList(Role.ROLE_USER))
+                .build();
+        visitorRepository.save(newVisitor);
+
+        List<String> roles = Collections.singletonList(Role.ROLE_USER.toString());
+        String token = jwtTokenServices.createToken(username, roles);
+        model.put("correct", true);
+        model.put("username", username);
+        model.put("roles", roles);
+        model.put("token", token);
+        return ResponseEntity.ok(model);
+    }
+
+    private ResponseEntity validateRegistryData(UserCredentials userCredentials) {
+        String username = userCredentials.getUsername();
+        String password = userCredentials.getPassword();
+        String firstname = userCredentials.getFirstname();
+        String lastname = userCredentials.getLastname();
+        String email = userCredentials.getEmail();
+        Gender gender = userCredentials.getGender();
+        List<String> errorList = new ArrayList<>();
+
+        if (visitorRepository.findByUsername(username).isPresent()) {
+            return failedRegisterMessage("Username already exists! Please choose a different username!");
+        }
+
+        if (!dataValidator.isValidUsername(username, errorList)) {
+            String error = String.join(" , ", errorList);
+            String errorMessage = "Username should have " + error + "!";
+            return failedRegisterMessage(errorMessage);
+        }
+
+        if (!dataValidator.isValidPassword(password, errorList)) {
+            String error = String.join(" , ", errorList);
+            String errorMessage = "Password should contain " + error + "!";
+            return failedRegisterMessage(errorMessage);
+        }
+
+        if (!dataValidator.isValidName(firstname, errorList)) {
+            String error = String.join(" , ", errorList);
+            String errorMessage = "Firstname " + error + "!";
+            return failedRegisterMessage(errorMessage);
+        }
+
+        if (!dataValidator.isValidName(lastname, errorList)) {
+            String error = String.join(" , ", errorList);
+            String errorMessage = "Lastname " + error + "!";
+            return failedRegisterMessage(errorMessage);
+        }
+
+        if (!dataValidator.isValidEmail(email, errorList)) {
+            return failedRegisterMessage("Not valid email!");
+        }
+
+        if (!dataValidator.isValidGender(gender, errorList)) {
+            return failedRegisterMessage("Not a vaild gender!");
+        }
+        return null;
+    }
+
+    private ResponseEntity failedRegisterMessage(String message) {
+        Map<String, Object> responseMessage = new HashMap<>();
+        responseMessage.put("correct", false);
+        responseMessage.put("msg", message);
+        return ResponseEntity.ok(responseMessage);
+    }
 }
