981746870ea57feb8ba9a391cc308a7514c0623f
dr. Simon Bálint <balladin0@gmail.com>
2020-08-14 00:05:39 +0200

    Fix show initializer by commenting out
    Initialization process produces NPE error: restTemplate would be needed before CommandLineRunner can run.
    Should solve problem by either rearrangement of initialization sequence or moving to other service

--- moviecatalog/src/main/java/com/drbsimon/moviecatalog/MoviecatalogApplication.java
+++ moviecatalog/src/main/java/com/drbsimon/moviecatalog/MoviecatalogApplication.java
@@ -1,19 +1,11 @@
 package com.drbsimon.moviecatalog;
 
-import com.drbsimon.moviecatalog.entity.Show;
-import com.drbsimon.moviecatalog.model.Movie;
-import com.drbsimon.moviecatalog.repository.ShowRepository;
-import com.drbsimon.moviecatalog.service.MovieServiceCaller;
-import com.drbsimon.moviecatalog.service.RoomServiceCaller;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -21,17 +13,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 @SpringBootApplication
 @EnableSwagger2
 @EnableEurekaClient
-@RequiredArgsConstructor
 public class MoviecatalogApplication {
-    private final ShowRepository showRepository;
-    private final RoomServiceCaller roomServiceCaller;
-    private final MovieServiceCaller movieServiceCaller;
 
     public static void main(String[] args) {
         SpringApplication.run(MoviecatalogApplication.class, args);
@@ -53,25 +38,32 @@ public class MoviecatalogApplication {
         return new RestTemplate();
     }
 
-    @Bean
-    @Profile("production")
-    public CommandLineRunner init() {
-        return args -> {
-            LocalDate fromDate = LocalDate.now();
-            for (int i = 0; i < 7; i++) {
-                LocalTime startingTime = LocalTime.of(12, 0);
-                for (Movie movie : movieServiceCaller.getAllShows()) {
-                    Show currentShow = Show.builder()
-                            .movieId(movie.getId())
-                            .startingDate(fromDate)
-                            .startingTime(startingTime)
-                            .roomId(roomServiceCaller.getAllRooms().get(0).getId())
-                            .build();
-                    showRepository.save(currentShow);
-                    startingTime = startingTime.plusHours(2);
-                }
-                fromDate = fromDate.plusDays(1);
-            }
-        };
-    }
+
+    // TODO: create shows should run after services are running
+    // NB: this code runs into NPE because restTemplate() is needed before it is initialized in main
+//    @Bean
+//    @Profile("production")
+//    public CommandLineRunner init() {
+//        return args -> {
+//            movieServiceCaller.test();
+//            LocalDate fromDate = LocalDate.now();
+//            for (int i = 0; i < 7; i++) {
+//                LocalTime startingTime = LocalTime.of(12, 0);
+//                List<Movie> movies = movieServiceCaller.getAllShows();
+//                if (movies != null) {
+//                    for (Movie movie : movies) {
+//                        Show currentShow = Show.builder()
+//                                .movieId(movie.getId())
+//                                .startingDate(fromDate)
+//                                .startingTime(startingTime)
+//                                .roomId(roomServiceCaller.getAllRooms().get(0).getId())
+//                                .build();
+//                        showRepository.save(currentShow);
+//                        startingTime = startingTime.plusHours(2);
+//                    }
+//                }
+//                fromDate = fromDate.plusDays(1);
+//            }
+//        };
+//    }
 }
--- moviecatalog/src/main/java/com/drbsimon/moviecatalog/service/MovieServiceCaller.java
+++ moviecatalog/src/main/java/com/drbsimon/moviecatalog/service/MovieServiceCaller.java
@@ -1,25 +1,30 @@
 package com.drbsimon.moviecatalog.service;
 
 import com.drbsimon.moviecatalog.model.Movie;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@CrossOrigin
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class MovieServiceCaller {
-
     private final RestTemplate restTemplate;
 
-    @Value("http://movieservice/movieservice")
+    @Value("${movieservice.url}")
     private String movieBaseUrl;
 
-    public List<Movie> getAllShows() {
-        return restTemplate.getForObject(movieBaseUrl + "/room", List.class);
+    // YAGNI: would have been used to initiate DB
+    public List<Movie> getAllMovies() {
+        return restTemplate.getForObject(movieBaseUrl + "/movie", List.class);
     }
 }
