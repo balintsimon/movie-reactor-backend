e7f7176732c7e06f5eadff3d5bfa990c76db59c4
dr. Simon Bálint <balladin0@gmail.com>
2020-08-18 14:30:39 +0200

    Add sample data provider

---
+++ moviecatalog/src/main/java/com/drbsimon/moviecatalog/data_sample/ShowCreator.java
@@ -0,0 +1,49 @@
+package com.drbsimon.moviecatalog.data_sample;
+
+import com.drbsimon.moviecatalog.entity.Show;
+import com.drbsimon.moviecatalog.model.Movie;
+import com.drbsimon.moviecatalog.model.Room;
+import com.drbsimon.moviecatalog.repository.ShowRepository;
+import com.drbsimon.moviecatalog.service.MovieServiceCaller;
+import com.drbsimon.moviecatalog.service.RoomServiceCaller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Component
+public class ShowCreator {
+    private final ShowRepository showRepository;
+    private final MovieServiceCaller movieServiceCaller;
+    private final RoomServiceCaller roomServiceCaller;
+
+    public ShowCreator(ShowRepository showRepository, MovieServiceCaller movieServiceCaller, RoomServiceCaller roomServiceCaller) {
+        this.showRepository = showRepository;
+        this.movieServiceCaller = movieServiceCaller;
+        this.roomServiceCaller = roomServiceCaller;
+    }
+
+    public void createWeeklyScheduleData(LocalDate fromDate) {
+        List<Movie> movies = movieServiceCaller.getAllMovies();
+        List<Room> rooms = roomServiceCaller.getAllRooms();
+
+        for (int i = 0; i < 7; i++) {
+            LocalTime startingTime = LocalTime.of(12, 0);
+
+            for (Movie movie : movies) {
+                Show currentShow = Show.builder()
+                        .movieId(movie.getId())
+                        .startingDate(fromDate)
+                        .startingTime(startingTime)
+                        .roomId(rooms.get(0).getId())
+                        .build();
+                showRepository.save(currentShow);
+                startingTime = startingTime.plusHours(2);
+            }
+            fromDate = fromDate.plusDays(1);
+        }
+    }
+}
