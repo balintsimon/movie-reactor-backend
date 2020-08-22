79e50e0055ca441a9d2a93e04bf420e8b87f5929
dr. Simon Bálint <balladin0@gmail.com>
2020-08-22 20:40:40 +0200

    Add get all visitors caller

--- booking/src/main/java/com/drbsimon/booking/service/VisitorServiceCaller.java
+++ booking/src/main/java/com/drbsimon/booking/service/VisitorServiceCaller.java
@@ -1,18 +1,29 @@
 package com.drbsimon.booking.service;
 
+import com.drbsimon.booking.model.Visitor;
+import com.drbsimon.booking.model.VisitorListWrapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @CrossOrigin
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class VisitorCaller {
+public class VisitorServiceCaller {
+
+    private final RestTemplate restTemplate;
 
     @Value("${visitor.url}")
     private String baseUrl;
-    // apigateway
+
+    public List<Visitor> getAllVisitors() {
+        VisitorListWrapper visitors = restTemplate.getForObject(baseUrl + "/user", VisitorListWrapper.class);
+        return visitors.getVisitors();
+    }
 }
