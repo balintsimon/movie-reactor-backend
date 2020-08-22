6d271cad1aa513aa3b9a0546648aa2109e0240a0
dr. Simon Bálint <balladin0@gmail.com>
2020-08-22 20:35:57 +0200

    Add working Room model to booking

--- booking/src/main/java/com/drbsimon/booking/model/RoomListWrapper.java
+++ booking/src/main/java/com/drbsimon/booking/model/RoomListWrapper.java
@@ -1,6 +1,5 @@
-package com.drbsimon.cinema.model;
+package com.drbsimon.booking.model;
 
-import com.drbsimon.cinema.entity.Room;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
