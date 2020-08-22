2b4249914e236ec561633c157a108aeb7fedc722
dr. Simon Bálint <balladin0@gmail.com>
2020-08-22 20:32:18 +0200

    Add sample data initialization

--- booking/src/main/java/com/drbsimon/booking/data_sample/BookingCreator.java
+++ booking/src/main/java/com/drbsimon/booking/data_sample/BookingCreator.java
@@ -1,4 +1,57 @@
 package com.drbsimon.booking.data_sample;
 
+import com.drbsimon.booking.entity.Reservation;
+import com.drbsimon.booking.model.*;
+import com.drbsimon.booking.repository.ReservationRepository;
+import com.drbsimon.booking.service.CatalogServiceCaller;
+import com.drbsimon.booking.service.CinemaServiceCaller;
+import com.drbsimon.booking.service.VisitorServiceCaller;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Component
 public class BookingCreator {
+
+    private final ReservationRepository reservationRepository;
+    private final CatalogServiceCaller catalogServiceCaller;
+    private final CinemaServiceCaller cinemaServiceCaller;
+    private final VisitorServiceCaller visitorServiceCaller;
+    private final Random random = new Random();
+
+    public BookingCreator(ReservationRepository reservationRepository,
+                          CatalogServiceCaller catalogServiceCaller,
+                          CinemaServiceCaller cinemaServiceCaller,
+                          VisitorServiceCaller visitorServiceCaller) {
+        this.reservationRepository = reservationRepository;
+        this.catalogServiceCaller = catalogServiceCaller;
+        this.cinemaServiceCaller = cinemaServiceCaller;
+        this.visitorServiceCaller = visitorServiceCaller;
+    }
+
+    public void createBookings() {
+        List<Show> shows = catalogServiceCaller.getAllShows();
+        List<Visitor> visitors = visitorServiceCaller.getAllVisitors();
+        List<Visitor> users = visitors.stream()
+                .filter(visitor -> visitor.getRoles().contains(Role.ROLE_USER))
+                .collect(Collectors.toList());
+        Visitor loneUser = users.get(0);
+
+        for (Show show : shows) {
+            Room room = cinemaServiceCaller.getRoomById(show.getRoomId());
+
+            for (Seat seat : room.getSeats()) {
+                if (random.nextBoolean()) {
+                    Reservation newReservation = Reservation.builder()
+                            .visitorId(loneUser.getId())
+                            .showId(show.getId())
+                            .seatId(seat.getId())
+                            .build();
+                    reservationRepository.save(newReservation);
+                }
+            }
+        }
+    }
 }
--- booking/src/main/java/com/drbsimon/booking/data_sample/DataProvider.java
+++ booking/src/main/java/com/drbsimon/booking/data_sample/DataProvider.java
@@ -1,26 +1,19 @@
-package com.drbsimon.moviecatalog.data_sample;
+package com.drbsimon.booking.data_sample;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-
-//@Component
 @Service
 public class DataProvider implements CommandLineRunner {
 
-    private final ShowCreator showCreator;
+    private final BookingCreator bookingCreator;
 
-    public DataProvider(ShowCreator showCreator) {
-        this.showCreator = showCreator;
+    public DataProvider(BookingCreator bookingCreator) {
+        this.bookingCreator = bookingCreator;
     }
 
     @Override
     public void run(String... args) {
-//        showCreator.createWeeklyScheduleData(LocalDate.of(2020, 7, 30));
-        LocalDate now = LocalDate.now();
-        System.out.println("Local Date now is: " + now);
-        showCreator.createWeeklyScheduleData(now);
+        bookingCreator.createBookings();
     }
 }
