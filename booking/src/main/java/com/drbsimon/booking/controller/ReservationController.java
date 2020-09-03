c6cd608564072ec54263645ecd9c47c31f075ca0
dr. Simon Bálint <balladin0@gmail.com>
2020-09-03 15:47:33 +0200

    Add endpoint to get all reservation data to logged-in user

--- booking/src/main/java/com/drbsimon/booking/controller/ReservationController.java
+++ booking/src/main/java/com/drbsimon/booking/controller/ReservationController.java
@@ -1,21 +1,27 @@
 package com.drbsimon.booking.controller;
 
 import com.drbsimon.booking.entity.SeatReservedWrapper;
-import com.drbsimon.booking.repository.ReservationRepository;
-import com.drbsimon.booking.service.ReservationOrganizer;
+import com.drbsimon.booking.model.AllBookingInfoWrapper;
+import com.drbsimon.booking.model.ReservationWrapper;
+import com.drbsimon.booking.repository.ReservationOrganizer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
 @RequestMapping("/reservation")
 @Slf4j
 @RequiredArgsConstructor
 public class ReservationController {
     private final ReservationOrganizer reservationOrganizer;
+
+    @GetMapping("/reservation")
+    public AllBookingInfoWrapper getAllReservations() {
+        return reservationOrganizer.getReservationsWithExtraInfoFactory();
+    }
 
     @Transactional
 //    @PostMapping("/seats") // TODO: rewrite frontend endpoint
@@ -33,9 +39,15 @@ public class ReservationController {
     // TODO: information must be combined in a service, rewrite
     // NB: doesn't work anymore, services are not linked!
 //    @GetMapping("/seats")
-//    // TODO: Only for admin, user should reach only user's seats! Secure endpoint
-//    public List<SeatReservedWithDetails> getAllReservedSeats() {
-//        return reservationRepository.getAllReservationsWithDetails();
-//    }
+//    // TODO: Only for admin, user internally, user should reach only user's seats! Secure endpoint
+    @GetMapping("/user")
+    public ReservationWrapper getAllReservedSeats() {
+        return reservationOrganizer.getAllReservations();
+    }
+
+    @GetMapping("/user/{id}")
+    public ReservationWrapper getReservationsOfUser(@PathVariable("id") Long id) {
+        return reservationOrganizer.getAllReservationsOfUserWithExtraInfo(id);
+    }
 
 }
