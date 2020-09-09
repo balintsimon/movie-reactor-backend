41a4c71954a2a8722228db5808b109eaf5a9a9d1
dr. Simon Bálint <balladin0@gmail.com>
2020-09-09 18:04:22 +0200

    Add test for movie manager

--- movieservice/src/test/java/com/drbsimon/movieservice/MovieserviceApplicationTests.java
+++ movieservice/src/test/java/com/drbsimon/movieservice/MovieserviceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.drbsimon.movieservice;
 
+import com.drbsimon.movieservice.controller.MovieController;
 import com.drbsimon.movieservice.entity.Movie;
 import com.drbsimon.movieservice.model.MovieListWrapper;
 import com.drbsimon.movieservice.repository.MovieManager;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,10 +24,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith({SpringExtension.class})
 @DataJpaTest
 @ActiveProfiles("test")
+@ComponentScan(basePackages = {"com.drbsimon.movieservice"})
 class MovieserviceApplicationTests {
 
     @Autowired
     private MovieRepository movieRepository;
+
+
+    @Autowired
+    private MovieManager movieManager;
+
+    @Test
+    public void testContexLoads() throws Exception {
+        assertThat(movieRepository).isNotNull();
+        assertThat(movieManager).isNotNull();
+    }
 
     @Test
     public void testSaveOneMovieSimple() {
@@ -72,7 +85,9 @@ class MovieserviceApplicationTests {
         movieRepository.saveAll(Arrays.asList(movie1, movie2, movie3));
         List<Movie> movies = movieRepository.findAll();
         Movie expectedResult = movies.get(0);
-        Movie actualResult = movieRepository.getById(expectedResult.getId());
+        Long sampleId = expectedResult.getId();
+        Movie actualResult = movieRepository.getById(sampleId);
+
         assertThat(actualResult).isEqualTo(expectedResult);
     }
 
@@ -85,5 +100,25 @@ class MovieserviceApplicationTests {
             movieRepository.save(movie);
             List<Movie> movies = movieRepository.findAll();
         });
+    }
+
+    @Test
+    public void testManager() {
+        Movie movie1 = Movie.builder()
+                .movieDbId(1234)
+                .build();
+
+        Movie movie2 = Movie.builder()
+                .movieDbId(5678)
+                .build();
+
+        Movie movie3 = Movie.builder()
+                .movieDbId(910)
+                .build();
+        movieRepository.saveAll(Arrays.asList(movie1, movie2, movie3));
+        List<Movie> expectedResult = movieRepository.findAll();
+        MovieListWrapper wrapper = movieManager.getAllMovies();
+        List<Movie> actualResult = wrapper.getMovies();
+        assertThat(actualResult).isEqualTo(expectedResult);
     }
 }
