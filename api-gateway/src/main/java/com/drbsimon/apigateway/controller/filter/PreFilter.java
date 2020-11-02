29de1dcc9970e0c3a019202c2f4817d1d7976dcc
dr. Simon Bálint <balladin0@gmail.com>
2020-11-02 17:50:27 +0100

    Refactor to use one service method only

--- api-gateway/src/main/java/com/drbsimon/apigateway/controller/filter/PreFilter.java
+++ api-gateway/src/main/java/com/drbsimon/apigateway/controller/filter/PreFilter.java
@@ -1,16 +1,20 @@
 package com.drbsimon.apigateway.controller.filter;
 
+import com.drbsimon.apigateway.model.entity.Visitor;
 import com.drbsimon.apigateway.security.CustomUserDetailsService;
+import com.drbsimon.apigateway.security.JwtTokenFilter;
+import com.drbsimon.apigateway.security.service.JwtTokenServices;
+import com.drbsimon.apigateway.service.dao.VisitorDao;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class PreFilter extends ZuulFilter {
     private final CustomUserDetailsService customUserDetailsService;
-
-    public PreFilter(CustomUserDetailsService customUserDetailsService) {
-        this.customUserDetailsService = customUserDetailsService;
-    }
 
     @Override
     public String filterType() {
@@ -30,9 +34,8 @@ public class PreFilter extends ZuulFilter {
     @Override
     public Object run() throws ZuulException {
         RequestContext ctx = RequestContext.getCurrentContext();
-        String username = customUserDetailsService.findLoggedInUsername();
-        Long userId = customUserDetailsService.findVisitorIdByUsername(username);
-        String foundUserId = (userId != null) ?  userId.toString() : null;
+        Visitor visitor = customUserDetailsService.parseVisitorFromToken();
+        String foundUserId = (visitor != null) ?  visitor.getId().toString() : null;
         ctx.addZuulRequestHeader("userid", foundUserId);
         return null;
     }
